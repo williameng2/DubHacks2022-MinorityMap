689b9ec140d75c49571237e1a6d6be2c9d875c17
Annika Singh <annikasingh@openexa.com>
2022-10-22 20:40:01 -0700

    map is owrking, added images of us
    Co-authored-by: William Eng <williameng2@users.noreply.github.com>
    Co-authored-by: nehapinni <nehapinni@users.noreply.github.com>

---
+++ Map/script.js
@@ -0,0 +1,30 @@
+var detailsBox = document.getElementById('details-box');
+
+document.addEventListener('mouseover', function (e) {
+  if (e.target.tagName == 'path') {
+    var content = e.target.dataset.name;
+    detailsBox.innerHTML = content;
+    detailsBox.style.opacity = "100%";
+  }
+  else {
+    detailsBox.style.opacity = "0%";
+  }
+});
+
+document.addEventListener("click", function (f) {
+    if (f.target.tagName == "path") {
+        var content = e.target.dataset.name;
+        detailsBox
+    }
+});
+
+window.onmousemove = function (e) {
+  var x = e.clientX,
+      y = e.clientY;
+  detailsBox.style.top = (y + 20) + 'px';
+  detailsBox.style.left = (x) + 'px';
+};
+
+window.onclick = function (f) {
+    sideInfo.style.
+}
