759d4f15b472fce1ffdca955c056d27d2c62466e
Annika Singh <annikasingh@openexa.com>
2022-10-22 23:05:35 -0700

    everything but the data
    Co-authored-by: William Eng <williameng2@users.noreply.github.com>

--- Map/script.js
+++ Map/script.js
@@ -1,4 +1,5 @@
 var detailsBox = document.getElementById('details-box');
+var sideInfo = document.getElementById('side-info');
 
 document.addEventListener('mouseover', function (e) {
   if (e.target.tagName == 'path') {
@@ -12,10 +13,16 @@ document.addEventListener('mouseover', function (e) {
 });
 
 document.addEventListener("click", function (f) {
-    if (f.target.tagName == "path") {
-        var content = e.target.dataset.name;
-        detailsBox
-    }
+  if (f.target.tagName == "path") {
+      var infocontent = f.target.dataset.info;
+      sideInfo.innerHTML = infocontent;
+      sideInfo.style.opacity = "100%";
+      sideInfo.style.zIndex = 1;
+  }
+  else {
+    sideInfo.style.opacity = "0%";
+    sideInfo.style.zIndex = 0;
+  }
 });
 
 window.onmousemove = function (e) {
@@ -25,6 +32,3 @@ window.onmousemove = function (e) {
   detailsBox.style.left = (x) + 'px';
 };
 
-window.onclick = function (f) {
-    sideInfo.style.
-}
